9207ae37f7c3fe8ffdfa393a2ca281e80d593985
mihai <mih.gogu@gmail.com>
2022-09-19 17:57:23 +0300

    Return composer-error output false if no issues detected

--- composer.sh
+++ composer.sh
@@ -21,7 +21,6 @@ echo -e "${BL}Info:${NC} Checking for lock file..."
 if [ ! -f "composer.lock" ]; then
   errorMessage="composer.lock file not found! Please commit your composer.lock file to your repository."
   echo "::error::$errorMessage"
-  echo "::set-output name=composer-error::true"
   echo "::set-output name=composer-error-message::$errorMessage"
   composerError=true
 else
@@ -35,14 +34,16 @@ if [ "$COMPOSER_CHECK_EXIT_CODE" != "0" ]; then
   encodedOutput=$(python3 -c "content='''$validateOutput''';print(content.replace(\"\r\", \"%0A\").replace(\"\n\", \"%0A\").replace(\"'\", \"\").replace(\"\\\"\", \"\").replace(\"\`\", \"\"))")
   errorMessage="Composer validation has errors... Check Composer Install logs for more info.%0A$encodedOutput"
   echo "::error::$errorMessage"
-  echo "::set-output name=composer-error::true"
   echo "::set-output name=composer-error-message::$errorMessage"
   composerError=true
 fi
 
 if [ "$composerError" = true ]; then
+  echo "::set-output name=composer-error::true"
   exit 1
 fi
+
+echo "::set-output name=composer-error::false"
 
 echo -e "${BL}Info:${NC} Running composer install.."
 docker exec php-container bash -c "$COMPOSER_COMMAND"
